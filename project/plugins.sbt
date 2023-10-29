8be5f640295c9e81a8164c4b350941ca49d99439
joesan <codeintheopen@gmail.com>
2023-10-29 07:50:24 +0100

    'aded dependencies'

---
+++ project/plugins.sbt
@@ -0,0 +1,2 @@
+// The plugin to create header license in source files
+addSbtPlugin("de.heikoseeberger" % "sbt-header" % "5.6.0")
--- src/main/scala/com/openelectrons/optimizer/Main.scala
+++ src/main/scala/com/openelectrons/optimizer/Main.scala
@@ -1,4 +1,6 @@
 package com.openelectrons.optimizer
+
+
 object Main extends App {
   println("Hello, World! This is a generated project template for open-electrons")
 }
